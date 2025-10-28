0ac55ce6c17b136eb3087cc57f4011aac5b2e546
MichaelChow-0604 <michael.chow@stoneleighsolution.com>
2025-10-28 17:22:15 +0800

    MODIFIED: Forget password cant be called without verify email

--- components/auth/sign-in.tsx
+++ components/auth/sign-in.tsx
@@ -137,11 +137,20 @@ export default function SignIn({ setIsSignIn }: SignInProps) {
         });
       },
       onError: ({ status }) => {
-        if (status === 400) {
-          toast.error("User not found.");
-          return;
+        switch (status) {
+          case 400:
+            toast.error("User not found.");
+            break;
+
+          case 403:
+            toast.error(
+              "Your account is unverified. Please sign up again to complete the verification process."
+            );
+            break;
+
+          default:
+            toast.error("Something went wrong. Please try again later.");
         }
-        toast.error("Something went wrong. Please try again later.");
       },
     }
   );
